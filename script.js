73cfc73daaae016a8398fbdaa72a35e4bdd441fc
wacky6382 <wacky6382@gmail.com>
2024-06-16 16:30:20 +0800

    Update script.js

--- script.js
+++ script.js
@@ -9,12 +9,44 @@ function calculateScore() {
 
     let score = 0;
 
-    if (temperature > 39) score += 2;
-    if (age > 65) score += 1;
-    if (heartRate > 100) score += 1;
-    if (wbcCount > 12000) score += 1;
+    if (temperature >= 39) score += 2;
+    if (age >= 65) score += 1;
+    if (heartRate >= 100) score += 1;
+    if (wbcCount >= 12000) score += 1;
     if (plateletCount < 150000) score += 1;
-    if (crp > 40) score += 1;
+    if (crp >= 40) score += 1;
 
-    document.getElementById('result').innerText = "Score: " + score;
+    let bacteremiaRate;
+
+    switch(score) {
+        case 0:
+            bacteremiaRate = "2.96%";
+            break;
+        case 1:
+            bacteremiaRate = "4.06%";
+            break;
+        case 2:
+            bacteremiaRate = "8.73%";
+            break;
+        case 3:
+            bacteremiaRate = "13.32%";
+            break;
+        case 4:
+            bacteremiaRate = "20.87%";
+            break;
+        case 5:
+            bacteremiaRate = "28.09%";
+            break;
+        case 6:
+            bacteremiaRate = "35.5%";
+            break;
+        case 7:
+            bacteremiaRate = "47.37%";
+            break;
+        default:
+            bacteremiaRate = "Invalid score";
+            break;
+    }
+
+    document.getElementById('result').innerText = "Score: " + score + "\nBacteremia Rate: " + bacteremiaRate;
 }
